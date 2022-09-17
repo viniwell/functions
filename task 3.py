dd12b99fbc294d05b5e1efadb96429433fbc88c4
viniwell <viniwell2807@gmail.com>
2022-09-17 19:32:38 +0100

    task 1

---
+++ task 3.py
@@ -0,0 +1,16 @@
+def get_analytics(s):
+    analytics={}
+    memory=[]
+    for i in list(s):
+        if i not in memory: 
+            analytics[i]=0
+            analytics[i]+=1
+            memory.append(i)
+        else:
+            analytics[i]+=1
+
+
+
+    return analytics
+print (get_analytics(input("write sentense-->")))
+         
