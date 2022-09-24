db1a947437d60c88f2b49feb510cb0e2f30a8235
viniwell <viniwell2807@gmail.com>
2022-09-24 14:31:57 +0100

    task 3.py

--- task 3.py
+++ task 3.py
@@ -2,6 +2,7 @@ def get_analytics(s):
     analytics={}
     memory=[]
     for i in list(s):
+        i=i.lower()
         if i not in memory: 
             analytics[i]=0
             analytics[i]+=1
