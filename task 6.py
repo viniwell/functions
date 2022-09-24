c3d50ce59add3c32d6cbb51cc4180291bba8eff0
viniwell <viniwell2807@gmail.com>
2022-09-24 11:11:32 +0100

    task 6.py

--- task 6.py
+++ task 6.py
@@ -4,16 +4,17 @@ def analyt(a,c=0):
     for i in a:
         if i!=0:
             if i not in b.keys():
-                b[int(i)]=''
-                b[int(i)]+=str(1)
+                b[str(i)]=''
+                b[str(i)]+=str(1)
                 c+=1
             else:
-                b[int(i)]+=str(1)
+                b[str(i)]+=str(1)
                 c+=1
             memory=[]
-            if i not in memory:
-                memory.append(i)
-                b[str(i)]=str(len(b[i]))+str('|')+str(f'{len(b[1])/c*100}:.1')+str('%')
+    for i in a:
+        if i not in memory:
+            memory.append(i)
+            b[str(i)]=str(len(b[i]))+str('|')+str(f'{len(b[1])/c*100}:.1')+str('%')
     return b
 print(analyt(input('input analytics----->')))
     
